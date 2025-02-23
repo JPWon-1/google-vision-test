2d61c929bbea7be687c0a153d2858c8cf4bc8425
JP Won <rocknzero1@gmail.com>
2025-02-23 22:41:08 +0900

    미리보기, 여러 파일 업로드 컨트롤러 생성

--- src/main/java/com/example/demo/HomeController.java
+++ src/main/java/com/example/demo/HomeController.java
@@ -15,10 +15,11 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.net.MalformedURLException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.Comparator;
-import java.util.List;
+import java.util.*;
 
 @RestController
 @CrossOrigin(origins = "*", methods = {RequestMethod.GET, RequestMethod.POST})
@@ -77,7 +78,7 @@ public class HomeController {
             Path filePath = Paths.get(absolutePath).normalize();
             Resource resource = new UrlResource(filePath.toUri());
 
-            System.out.print("absolute File Path :" + absolutePath);
+            System.out.println("absolute File Path :" + absolutePath);
             if (!resource.exists() || !resource.isReadable()) {
                 throw new FileNotFoundException("File not found after upload: " + file.getOriginalFilename());
             }
@@ -94,9 +95,94 @@ public class HomeController {
                     .body(resource);
 
         } catch (Exception e) {
-            System.out.print("Failed to extract text: " + e.getMessage());
+            System.out.println("Failed to extract text: " + e.getMessage());
             e.printStackTrace();
             return null;
         }
+    }
+
+    @PostMapping("/uploads")
+    public ResponseEntity<?> updateGpsDatas(@RequestParam("files") List<MultipartFile> files) {
+        try {
+            List<Map<String, String>> resources = new ArrayList<>();
+
+            for (MultipartFile file : files) {
+                AnnotateImageResponse imageResponse = fileService.annotateImage(file);
+                List<EntityAnnotation> landmarkAnnotations = imageResponse.getLandmarkAnnotationsList();
+                EntityAnnotation highestScoreAnnotation = landmarkAnnotations.stream()
+                        .max(Comparator.comparing(EntityAnnotation::getScore))
+                        .orElse(null);
+
+                File updatedImageFile = null;
+                if (highestScoreAnnotation != null) {
+                    LocationInfo locations = highestScoreAnnotation.getLocations(0);
+                    LatLng latLng = locations.getLatLng();
+                    double longitude = latLng.getLongitude();
+                    double latitude = latLng.getLatitude();
+                    updatedImageFile = fileService.updateGpsData(file, longitude, latitude);
+                }
+                String absolutePath = updatedImageFile.getAbsolutePath();
+//                Path filePath = Paths.get(absolutePath).normalize();
+//                Resource resource = new UrlResource(filePath.toUri());
+
+//                System.out.println("absolute File Path :" + absolutePath);
+//                if (!resource.exists() || !resource.isReadable()) {
+//                    throw new FileNotFoundException("File not found after upload: " + file.getOriginalFilename());
+//                }
+                Map<String, String> resourceKV = new HashMap<>();
+                resourceKV.put("fileUrl", absolutePath); // 미리보기 URL용
+                resourceKV.put("fileName", file.getOriginalFilename()); // 다운받을 파일 ID
+                resources.add(resourceKV);
+            }
+
+            return ResponseEntity.ok()
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .body(resources);
+
+        } catch (Exception e) {
+            System.out.println("Failed to extract text: " + e.getMessage());
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+
+    @GetMapping("/download/{fileName}")
+    public ResponseEntity<Resource> downloadFile(@PathVariable String fileName) {
+        try {
+            Path filePath = Paths.get("/Users/jp/Pictures/test").resolve(fileName); // 서버의 실제 파일 경로
+            Resource resource = new UrlResource(filePath.toUri());
+
+            if (!resource.exists() || !resource.isReadable()) {
+                throw new FileNotFoundException("File not found " + fileName);
+            }
+
+
+            return ResponseEntity.ok()
+                    .contentType(MediaType.APPLICATION_OCTET_STREAM)
+                    .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + fileName + "\"")
+                    .body(resource);
+
+        } catch (IOException e) {
+            throw new RuntimeException("File not found", e);
+        }
+    }
+
+    /**
+     * 개별 파일 미리보기
+     */
+    @GetMapping("/preview/{fileName}")
+    public ResponseEntity<Resource> previewFile(@PathVariable String fileName) {
+        Path filePath = Paths.get("/Users/jp/Pictures/test").resolve(fileName); // 서버의 실제 파일 경로
+        try {
+            Resource resource = new UrlResource(filePath.toUri());
+            return ResponseEntity.ok()
+                    .contentType(MediaType.IMAGE_JPEG)
+                    .body(resource);
+        } catch (MalformedURLException e) {
+            throw new RuntimeException(e);
+        }
+
+
     }
 }
